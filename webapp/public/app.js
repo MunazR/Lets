40bd3add0c3ab6ed15ebff66fc9d9e6074e11851
munazr <munaz_rahman@hotmail.com>
2015-09-19 06:14:57 -0400

    Added categories and ability to create events

--- webapp/public/app.js
+++ webapp/public/app.js
@@ -1,7 +1,62 @@
 $(document).ready(function() {
   auth();
+
   $("#retry-auth").on('click', function() {
     auth();
+  });
+
+  $("#create-event-link").on('click', function() {
+    $("#create-event-form").show();
+    $("#categories-container").hide();
+  });
+
+  $("#create-event-back").on('click', function() {
+    $("#create-event-form").hide();
+    $("#categories-container").show();
+  });
+
+  $("#create-event-confirm").on('click', function() {
+    var name = $("#event-name").val();
+    var description = $("#event-description").val();
+    var location = $("#event-location").val();
+    var date = $("#event-date").val();
+    var time = $("#event-time").val();
+    var category = $("#event-category").val();
+
+    if (!name) {
+      return alert('Missing event name');
+    }
+
+    if (!description) {
+      return alert('Missing event description');
+    }
+
+    if (!location) {
+      return alert('Missing event location');
+    }
+
+    if (!date) {
+      return alert('Missing event date');
+    }
+
+    if (!time) {
+      return alert('Missing event time');
+    }
+
+    var eventsRef = new Firebase("https://incandescent-torch-8518.firebaseio.com/events");
+    var newEvent = {
+      name: name,
+      description: description,
+      location: location,
+      date: date,
+      time: time,
+      category: category
+    };
+    eventsRef.push(newEvent);
+
+    alert('Event created!');
+    $("#create-event-form").hide();
+    $("#categories-container").show();
   });
 });
 
@@ -28,4 +83,25 @@ function init(ref, data) {
 
   $("#user-name").text(profile.first_name);
   $("#user-image").attr('src', profile.picture.data.url);
+  $("#user-info").show();
+
+  var categoriesRef = ref.child('categories');
+
+  categoriesRef.on('value', function(dataSnapshot) {
+    var data = dataSnapshot.val();
+    var template = Handlebars.compile($("#category-template").html());
+    var category;
+
+    $("#categories").empty();
+
+    for (var categoryId in data) {
+      category = data[categoryId];
+      category.id = categoryId;
+      $("#categories").append(template(category));
+    }
+
+    $("#categories-container").show();
+  });
+
+  console.log('categories', categoriesRef);
 }
